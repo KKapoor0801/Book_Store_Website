3927ee3faa5560352820d754b4cf31f4b5b71dad
KKapoor0801 <58838197+KKapoor0801@users.noreply.github.com>
2022-06-03 03:10:53 +0530

    Backend of book store is ready

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require("./routes/book-routes");
+const cors = require("cors");
 const app = express();
 
 //Middlewares
 app.use(express.json());
+app.use(cors());
 app.use("/books",router) //localhost:5000/books
 
 mongoose.connect(
